f495909e52a5ec5d64f711665030a726cb4b9cc6
DedNikolai <nikolai.blashchuk@gmail.com>
2025-02-28 13:56:14 +0200

    add checkboxes

--- src/app/(root)/@modal/(.)product/[id]/page.tsx
+++ src/app/(root)/@modal/(.)product/[id]/page.tsx
@@ -1,10 +1,12 @@
 
 import { notFound } from 'next/navigation';
 import { prisma } from '../../../../../../prisma/prisma-client';
-import { ChooseProductModal } from '@/components/shared/modals/choose-product-modals';
+import { ChooseProductModal } from '@/shared/components/shared/modals/choose-product-modals';
 
-export default async function ProductModalPage({ params: { id } }: { params: { id: string } }) {
-  const product = await prisma.product.findFirst({
+export default async function ProductModalPage({ params }: { params: { id?: string } }) {
+    const { id } = await params;
+  
+    const product = await prisma.product.findFirst({
     where: {
       id: Number(id),
     },
---
+++ src/shared/components/shared/top-bar.tsx
@@ -0,0 +1,22 @@
+import { cn } from "@/shared/lib/utils";
+import { FC } from "react";
+import { Categories } from "./categories";
+import { SortPopUp } from "./sort-popup";
+import { Container } from "./container";
+import { Category } from "@prisma/client";
+
+interface Props {
+    categories: Category [];
+    className?: string
+}
+
+export const TopBar: FC<Props> = ({className, categories}) => {
+    return (
+        <div className={cn('sticky top-0 bg-white py-5 shadow-lg shadow-black/5 z-10', className)}>
+            <Container className="flex items-center justify-between">
+                <Categories items={categories}/>
+                <SortPopUp />
+            </Container>
+        </div>
+    )
+}
